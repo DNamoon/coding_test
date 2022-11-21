825d82b9f091ce96d93929a336890ebdc09b24c3
DNamoon <leekoo95@naver.com>
2022-11-22 03:14:58 +0900

    v.221121

---
+++ src/CountPAndY.java
@@ -0,0 +1,98 @@
+/** 22.11.21
+ * 출처: 프로그래머스 코딩 테스트 연습 lv0, https://school.programmers.co.kr/learn/courses/30/lessons/12916
+ * 대문자와 소문자가 섞여있는 문자열 s가 주어집니다. s에 'p'의 개수와 'y'의 개수를 비교해 같으면 True,
+ * 다르면 False를 return 하는 solution를 완성하세요. 'p', 'y' 모두 하나도 없는 경우는 항상 True를 리턴합니다.
+ * 단, 개수를 비교할 때 대문자와 소문자는 구별하지 않습니다.
+ *
+ * 예를 들어 s가 "pPoooyY"면 true를 return하고 "Pyy"라면 false를 return합니다.
+ *
+ * 제한사항
+ * 문자열 s의 길이 : 50 이하의 자연수
+ * 문자열 s는 알파벳으로만 이루어져 있습니다.
+ */
+public class CountPAndY {
+    static boolean solution(String s) {
+        boolean answer = true;
+        int n1 = 0;
+        int n2 = 0;
+
+        int i = 0;
+        char P = 80;
+        char p = 112;
+        char Y = 89;
+        char y = 121;
+
+        while (i<s.length()){
+            if(P == s.charAt(i)) {
+                n1++;
+                i++;
+                continue;
+            }
+            if(p == s.charAt(i)){
+                n1++;
+                i++;
+                continue;
+            }
+
+            if(Y == s.charAt(i)) {
+                n2++;
+                i++;
+                continue;
+            }
+            if(y == s.charAt(i)){
+                n2++;
+                i++;
+                continue;
+            }
+            i++;
+        }
+
+        if(n1 != n2) {
+            answer = false;
+        }
+
+        return answer;
+    }
+
+    //내가 한 것을 switch 문으로 바꿔보자
+    //이렇게 간단해 지는 것을.... char를 선언하고 싶다면 쌍따옴표 말고 홑따옴표로!!
+    static boolean solution2(String s) {
+
+        int pCount = 0;
+        int yCount = 0;
+
+        for(int i = 0; i<s.length(); i++){
+            switch (s.charAt(i)){
+                case 'p' : pCount++; break;
+                case 'P' : pCount++; break;
+                case 'y' : yCount++; break;
+                case 'Y' : yCount++; break;
+            }
+        }
+
+        return (pCount == yCount)?true:false;
+    }
+
+    //toUpperCase() 내장 함수 사용하기.
+    static boolean solution3(String s) {
+        int pCount = 0;
+        int yCount = 0;
+        String word = s.toUpperCase();
+
+        for(int i = 0; i < word.length(); i++){
+            switch (word.charAt(i)) {
+                case 'P' : pCount++; break;
+                case 'Y' : yCount++; break;
+            }
+        }
+
+        return pCount==yCount ? true : false;
+    }
+
+    public static void main(String[] args) {
+        boolean test1 = solution("pPoooyY");
+        System.out.println("test1 = " + test1);
+        boolean test2 = solution2("pPoooyY");
+        System.out.println("test2 = " + test2);
+    }
+}
---
+++ src/SquareRoot.java
@@ -0,0 +1,43 @@
+/**
+ * 출처: 프로그래머스 코딩 테스트 연습 lv0, https://school.programmers.co.kr/learn/courses/30/lessons/12934
+ * 임의의 양의 정수 n에 대해, n이 어떤 양의 정수 x의 제곱인지 아닌지 판단하려 합니다.
+ * n이 양의 정수 x의 제곱이라면 x+1의 제곱을 리턴하고, n이 양의 정수 x의 제곱이 아니라면 -1을 리턴하는 함수를 완성하세요.
+ */
+public class SquareRoot {
+    public long solution(long n) {  //정수 제곱근 판별
+        long answer = -1L;
+
+        //시간초과 걸림 n의 범위가 1이상 5xe13 이여서 그런가봄.
+//        int j=1;
+//        while(j*j<=n){
+//            if(j*j == n) {
+//                answer = (j+1)*(j+1);
+//                break;
+//            }
+//            j++;
+//        }
+//        for(int i=1; i*i <= n; i++){
+//            if(i*i == n){
+//                answer = (i+1)*(i+1);
+//            } else {
+//                answer = -1;
+//            }
+//        }
+
+        //다른 분 풀이
+//        if(Math.pow((int)Math.sqrt(n),2) == n ) {
+//            return (long) Math.pow(Math.sqrt(n)+1,2);
+//        }
+//
+//        return -1;
+
+        //함수 쓰기 싫었는데
+        Double sqrt = Math.sqrt(n);  //Double wrapper class에는 intValue() 정수형 숫자 변환 함수가 있다.
+        if(sqrt == sqrt.intValue()) {
+            //answer = (new Double((sqrt + 1)*(sqrt + 1))).longValue();
+            answer = (long) Math.pow(sqrt+1,2);
+        }
+
+        return answer;
+    }
+}
