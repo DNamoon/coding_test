5424e8aa90c2e5e638e9d635a7a4d1fffe833d25
Ho <leekoo3086@naver.com>
2022-12-20 22:51:50 +0900

    v.221220_programmers

---
+++ src/programmers/PlusOfArrays.java
@@ -0,0 +1,35 @@
+/**
+ * 출처 : 프로그래머스 코딩테스트 연습 lv1, https://school.programmers.co.kr/learn/courses/30/lessons/12950
+ * 22.12.20
+ *
+ * 행렬의 덧셈은 행과 열의 크기가 같은 두 행렬의 같은 행, 같은 열의 값을 서로 더한 결과가 됩니다.
+ * 2개의 행렬 arr1과 arr2를 입력받아, 행렬 덧셈의 결과를 반환하는 함수, solution을 완성해주세요.
+ *
+ * 제한 조건
+ * 행렬 arr1, arr2의 행과 열의 길이는 500을 넘지 않습니다.
+ */
+package programmers;
+
+import java.util.Arrays;
+
+public class PlusOfArrays {
+    public static int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr1[0].length];
+        for(int i=0; i< arr1.length; i++) {
+            for(int j=0; j< arr2[0].length; j++) {
+                answer[i][j] = arr1[i][j] + arr2[i][j];
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[][] arr1 = new int[][]{{1}, {2}};
+        int[][] arr2 = new int[][]{{4}, {6}};
+        System.out.println(arr1.length);
+        System.out.println(Arrays.toString(arr1[0]));
+        System.out.println(arr1[0].length);
+        System.out.println(Arrays.deepToString(solution(arr1,arr2)));
+
+    }
+}
